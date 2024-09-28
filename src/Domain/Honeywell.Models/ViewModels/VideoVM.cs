072fe4c1af18925c7cba117e781304c704414273
Kiet Pham <kietpham.dev@gmail.com>
2024-09-28 19:42:04 +1000

    Get FileSize in ViewVM

--- src/Domain/Honeywell.Models/ViewModels/VideoVM.cs
+++ src/Domain/Honeywell.Models/ViewModels/VideoVM.cs
@@ -1,3 +1,8 @@
+using Honeywell.Utility.Helpers;
+
 namespace Honeywell.Models.ViewModels;
 
-public record VideoVM(int Id, string FileName, string FilePath, decimal FileSizeInMb);
+public record VideoVM(int Id, string FileName, string FilePath, decimal FileSizeInMb)
+{
+    public string FileSize => CommonUtility.GetReadableFileSize(FileSizeInMb);
+}
