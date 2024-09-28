ed2d0dee9c99e65ae871420d51fbd02b7815e323
Kiet Pham <kietpham.dev@gmail.com>
2024-09-28 15:30:57 +1000

    Seeding db

--- src/Domain/Honeywell.DataAccess/Data/ApplicationDbContext.cs
+++ src/Domain/Honeywell.DataAccess/Data/ApplicationDbContext.cs
@@ -20,14 +20,14 @@ public class ApplicationDbContext : DbContext
         
         base.OnModelCreating(modelBuilder);
 
-        // modelBuilder.Entity<VideoFile>().HasData(
-        //     new VideoFile
-        //     {
-        //         Id = 1,
-        //         FileName = "big_buck_bunny",
-        //         FilePath = "media/big_buck_bunny/big_buck_bunny.mp4",
-        //         FileSize = 100
-        //     }
-        // );
+        modelBuilder.Entity<VideoFile>().HasData(
+            new VideoFile
+            {
+                Id = 1,
+                FileName = "big_buck_bunny",
+                FilePath = "media/big_buck_bunny/big_buck_bunny.mp4",
+                FileSizeInMb = 5510872 // 5.25 MB
+            }
+        );
     }
 }
